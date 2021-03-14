3c4db92a4774ee57fd35327ef452f8dfb2149c67
Denis Basharov <basharov@zirconb.com>
2021-03-14 13:09:50 +0300

    Добавил обработку CTRL-C для остановки двигателя

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 # ssh -t pi@192.168.0.12 'cd /home/pi/smartlab/src; python ./pca9685_servo.py'
 
-ssh -t pi@192.168.0.12 'cd /home/pi/smartlab/src; python ./pca9685_servo.py'
+ssh -t pi@192.168.1.247 'cd /home/pi/smartlab/src; python ./pca9685_servo.py'
+
+
