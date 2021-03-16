64df9621a1b413e2e0cd07d9ab97b5f5b3fd8629
Denis Basharov <basharov@zirconb.com>
2021-03-16 21:43:50 +0300

    Добавил обработку CTRL-C для остановки двигателя

--- src/pca9685_servo.py
+++ src/pca9685_servo.py
@@ -20,20 +20,20 @@ try:
     # time.sleep(1)
 
     # for value in range(100):
-    # while True:
-    #     servo.setPWM(wheelLeftFrontForward, 4096, 4096)
-    #     servo.setPWM(wheelRightFrontForward, 4096, 4096)
-    #     servo.setPWM(wheelLeftRearForward, 4096, 4096)
-    #     servo.setPWM(wheelRightRearForward, 4096, 4096)
-    #     time.sleep(0.01)
+    while True:
+        servo.setPWM(wheelLeftFrontForward, 4096, 0)
+        servo.setPWM(wheelRightFrontForward, 4096, 0)
+        servo.setPWM(wheelLeftRearForward, 4096, 0)
+        servo.setPWM(wheelRightRearForward, 4096, 0)
+        time.sleep(0.01)
     # #
     # time.sleep(3)
-    while True:
-        servo.setPWM(wheelLeftFrontReverse, 4096, 0)
-        servo.setPWM(wheelRightFrontReverse, 4096, 0)
-        servo.setPWM(wheelLeftRearReverse, 4096, 0)
-        servo.setPWM(wheelRightRearReverse, 4096, 0)
-        time.sleep(0.01)
+    # while True:
+    #     servo.setPWM(wheelLeftFrontReverse, 4096, 0)
+    #     servo.setPWM(wheelRightFrontReverse, 4096, 0)
+    #     servo.setPWM(wheelLeftRearReverse, 4096, 0)
+    #     servo.setPWM(wheelRightRearReverse, 4096, 0)
+    #     time.sleep(0.01)
     #
     # for value in range(100):
     #     servo.setServo(test_channel, 100 - value)
