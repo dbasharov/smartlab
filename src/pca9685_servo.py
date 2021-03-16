3062efdbb2d581bfe74c93e90d4df67a1e044ab5
Denis Basharov <basharov@zirconb.com>
2021-03-16 21:05:29 +0300

    Добавил обработку CTRL-C для остановки двигателя

--- src/pca9685_servo.py
+++ src/pca9685_servo.py
@@ -9,16 +9,16 @@ test_channel = 0
 
 try:
     servo.servos[test_channel].set(signed=False, reverse=False, min=100, max=100, trim=0, exp=100)
-    servo.setServo(test_channel, 0)
-    time.sleep(1)
+    servo.setPWM(test_channel, 0,1)
+    # time.sleep(1)
 
-    for value in range(100):
-        servo.setServo(test_channel, value)
-        time.sleep(0.01)
-
-    for value in range(100):
-        servo.setServo(test_channel, 100 - value)
-        time.sleep(0.01)
+    # for value in range(100):
+    #     servo.setServo(test_channel, value)
+    #     time.sleep(0.01)
+    #
+    # for value in range(100):
+    #     servo.setServo(test_channel, 100 - value)
+    #     time.sleep(0.01)
 except KeyboardInterrupt:
     print "Keyboard Interrupt"
 finally:
