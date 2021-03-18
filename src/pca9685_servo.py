ad036f0c68903001d4fb323c365364d3df8bd2e0
Denis Basharov <basharov@zirconb.com>
2021-03-18 22:32:28 +0300

    Добавил обработку CTRL-C для остановки двигателя

--- src/pca9685_servo.py
+++ src/pca9685_servo.py
@@ -27,7 +27,7 @@ try:
     while True:
         servo.setPWM(wheelLeftFrontForward, 4096, 0)
         # servo.setPWM(wheelRightFrontForward, 4096, 0)
-        servo.setPWM(wheelLeftRearForward, 800, 0)
+        servo.setPWM(wheelLeftRearForward, 1800, 0)
         # servo.setPWM(wheelRightRearForward, 4096, 0)
         # time.sleep(0.01)
 
