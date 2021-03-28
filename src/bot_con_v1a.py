dda52b73c03ec7e85ebe99b08a0ef8e6f7f8938c
Denis Basharov <basharov@zirconb.com>
2021-03-28 13:02:47 +0300

    Добавил обработку CTRL-C для остановки двигателя

--- src/bot_con_v1a.py
+++ src/bot_con_v1a.py
@@ -10,7 +10,7 @@ import Adafruit_PCA9685
 #pca9685 = PCA9685.PCA9685(i2cBus)
 #servo00 = ServoPCA9685.ServoPCA9685(pca9685, PCA9685.CHANNEL12)
 
-pwm = Adafruit_PCA9685.PCA9685(address=0x40) # по умолчанию, если не введен адрес устройства, используется адрес 0x40
+pwm = Adafruit_PCA9685.PCA9685(address=0x40) # (задаем переменну - обращение к контроллеру PWM -  по умолчанию, если не введен адрес устройства, используется адрес 0x40
 pwm.set_pwm_freq(50) # частота ШИМ-сигнала, равная 50Гц (20 мс)
 
 servo_min=310
@@ -20,7 +20,7 @@ servo_set=0
 spd_on = 4096
 spd_off = 0
 
-FL_on_F = FL_off_F = 0
+FL_on_F = FL_off_F = 0 # переднее левое включение/
 FL_on_B = FL_off_B = 0
 FR_on_F = FR_off_F = 0
 FR_on_B = FR_off_B = 0
@@ -71,7 +71,15 @@ while 1:
             elif event.key == pygame.K_3:
                 R3 = True
             elif event.key == pygame.K_4:
-                R4 = True   
+                R4 = True
+
+            if event.key in [pygame.K_SPACE]:  # общий стоп
+                flUp = flDown = flLeft = flRight = False
+                R1 = R2 = R3 = R4 = False
+                servo_set = 300  # сброс серв
+                x = W // 2
+                y = H // 2
+
         elif event.type == pygame.KEYUP:#проверка отжатия кнопки
             if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
                 flUp = flDown = flLeft = flRight = False
@@ -83,12 +91,7 @@ while 1:
                 BL_on_B = BL_off_B = 0
                 BR_on_F = BR_off_F = 0
                 BR_on_B = BR_off_B = 0
-            if event.key in [pygame.K_SPACE]:#общий стоп
-                flUp = flDown = flLeft = flRight = False
-                R1 = R2 = R3 = R4 = False
-                servo_set=300 #сброс серв
-                x = W // 2
-                y = H // 2
+
  
     if flLeft:#если была нажата кнопка "влево"
         x -= speed
