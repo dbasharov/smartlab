d852eb05c60156ebde1d6bd60a3ade4e9d4052cc
Denis Basharov <basharov@zirconb.com>
2021-03-28 14:15:59 +0300

    Добавил обработку CTRL-C для остановки двигателя

--- src/bot_con_v1a.py
+++ src/bot_con_v1a.py
@@ -17,12 +17,12 @@ servo_min=204
 servo_max=409
 servo_set_1=307
 servo_set_2=307
-#
 
-spd_on = 4096
-spd_off = 0
 
-FL_on_F = FL_off_F = 0 # переднее левое включение/
+spd_on = 4096 #проврить, удалить
+spd_off = 0 #проврить, удалить
+
+FL_on_F = FL_off_F = 0 # переменные, которые включают/выключают импульсы на колесах (ШИМ? - пары чисел из адафрут 0, 4096 и т.п.)переднее левое включение/
 FL_on_B = FL_off_B = 0
 FR_on_F = FR_off_F = 0
 FR_on_B = FR_off_B = 0
@@ -31,34 +31,34 @@ BL_on_B = BL_off_B = 0
 BR_on_F = BR_off_F = 0
 BR_on_B = BR_off_B = 0
 
-pygame.init()
+pygame.init() #инициализация опроса клавиатуры через Pygame
  
-W = 200
+W = 200 # установка размера графического окна, нужно ли - удалить?
 H = 200
  
-sc = pygame.display.set_mode((W, H))
+sc = pygame.display.set_mode((W, H)) #переменная инициализации окна - имя переменной устанавливаем сами
  
-WHITE = (255, 255, 255)
+WHITE = (255, 255, 255) # задание цветов графического окна
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
  
-FPS = 60   
-clock = pygame.time.Clock()
+FPS = 60   # обновление окна
+clock = pygame.time.Clock() # переменная цикла опроса клавиатуры
  
-x = W // 2
+x = W // 2 #установка координат центра окна для установки курсора. Проверить, надо? Выкинуть
 y = H // 2
-speed = 1
+speed = 1 # шаг перемещения курсора,  Проверить, надо? Выкинуть
  
-flUp = flDown = flLeft = flRight = False
-R1 = R2 = R3 = R4 = R5 = False
+flUp = flDown = flLeft = flRight = False # установка флагов (из механики Pygame) по типу чекбоксов, свои переменные и присвение им значение false, =0? проверить. Далее используются для управления стрелками с клавиатуры.
+R1 = R2 = R3 = R4 = R5 = False # тоже самое, только управление цифрами с клавиатуры.
 
-while 1:
+while 1: #запускаем общий цикл для всего - оптимизировать?
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # выход по какому условию? Нужен?
             exit()
         elif event.type == pygame.KEYDOWN:#проверка нажатия кнопки
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT: #обозначение клавиш из Pygame
                 flLeft = True
             elif event.key == pygame.K_RIGHT:
                 flRight = True
@@ -98,36 +98,52 @@ while 1:
                 BR_on_B = BR_off_B = 0
 
  
-    if flLeft:#если была нажата кнопка "влево"
-        x -= speed
+    if flLeft:# Левый танковый разворот. Если была нажата кнопка "влево"  (ПРОВЕРКА сосстояния - если ifLeft = true), то присваиваем значения переменным, отвечающим за направление вращения двигателем, значение скорости через параметры ШИМ.
+        x -= speed # изменение координаты курора в окне - "-1" - на шаг speed - задан выше (=1). То есть х = х-1 (х уменьшить на один). Х - это половина размера окна - оптимизировать или удалить всю графику? Может быть оставить для отображения положения, передвижения, состояния платформы.
         FL_on_B = 4096
         FR_on_F = 4096
         BL_on_B = 4096
         BR_on_F = 4096
       
-        
-    elif flRight:
+        #
+        #
+        #
+
+    elif flRight: # правый танковый разворот
         x += speed
-        FL_on_F = 4096
+        if FL_on_F <= 4096: #плавный старт передний левый двигатель
+            FL_on_F += 200
+        else:
+            FL_on_F = 4096
         FR_on_B = 4096
         BL_on_F = 4096
         BR_on_B = 4096
+#
+#
+#
+    # elif flRight:  # правый танковый разворот
+        # x += speed
+        # FL_on_F = 4096
+        # FR_on_B = 4096
+        # BL_on_F = 4096
+        # BR_on_B = 4096
+
         
-    elif flUp:
+    elif flUp: #все вперед
         y -= speed
         FL_on_F = 4096
         FR_on_F = 4096
         BL_on_F = 4096
         BR_on_F = 4096
                 
-    elif flDown:
+    elif flDown: # все назад
         y += speed
         FL_on_B = 4096 
         FR_on_B = 4096 
         BL_on_B = 4096
         BR_on_B = 4096
         
-    elif R1:#если была нажата кнопка "1"
+    elif R1:#если была нажата кнопка "1" - управление сервами через цыфры, переменные осей 1 и 2
         servo_set_1=204
         servo_set_2 = 204
     elif R2:
@@ -143,10 +159,12 @@ while 1:
     elif R5:
         servo_set_1 = 204
 
-    sc.fill(WHITE)
-    pygame.draw.rect(sc, BLUE, (x, y, 10, 10))
-    pygame.display.update()
-    pwm.set_pwm(4, FL_on_F, FL_off_F) #ПЛ_вперёд
+    sc.fill(WHITE) #окошко - нужно?
+    pygame.draw.rect(sc, BLUE, (x, y, 10, 10)) # размер курсора - нужно?
+    pygame.display.update() # обновление окошка - нужно?
+
+
+    pwm.set_pwm(4, FL_on_F, FL_off_F) #ПЛ_вперёд обращение к двигателю через номер порта и параметры ШИМ
     pwm.set_pwm(5, FL_on_B, FL_off_B) #ПЛ_назад
     pwm.set_pwm(6, FR_on_F, FR_off_F) #ПП_вперёд
     pwm.set_pwm(7, FR_on_B, FR_off_B) #ПП_назад
