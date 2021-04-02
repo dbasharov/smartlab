829599eb84228da24ead1976c78577d18b55d32e
Denis Basharov <basharov@zirconb.com>
2021-04-02 12:18:37 +0300

    Основная рабочая версия Управление механикой Используется: - Pygame - для управления с клавиатуры и джойстика - Adafruit PCA9685 library
    Изменены названия переменных, отвечающих за значение PWM, убраны лишние, значение PWM начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает:
    - двежение вперед и назад через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши курсора
    - параллельная парковка через клавиши 1 и 3
    - возврат серво в исходное положение пробелом или клавишей 2
    - управление серво через пробел - после любого из положений, сначала нажимаем пробел, затем другое положение

--- src/mechanical_control.py
+++ src/mechanical_control.py
@@ -236,15 +236,15 @@ while 1: # Запускаем общий цикл для всего - оптим
 
     # тестирование плавного поворота серво
     elif test_servo_left:
-        # test_servo_min = test_servo_min - 1
+        test_servo_min = test_servo_min + 1
         test_servo_pwm = test_servo_min
-        # time.sleep(0.3)
+        time.sleep(0.3)
         print ("servo right")
 
     elif test_servo_right:
-        # test_servo_max = test_servo_max + 1
+        test_servo_max = test_servo_max - 1
         test_servo_pwm = test_servo_max
-        # time.sleep(0.3)
+        time.sleep(0.3)
         print ("servo left")
 
         # test_servo_min = 154
