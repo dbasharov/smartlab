0b1c82ca9489a4d7806a924b9ed57bfdd9d72b05
Denis Basharov <basharov@zirconb.com>
2021-04-03 15:39:32 +0300

    Основная рабочая версия Управление механикой Используется: - Pygame - для управления с клавиатуры и джойстика - Adafruit PCA9685 library
    Изменены названия переменных, отвечающих за значение PWM, убраны лишние, значение PWM начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает:
    - двежение вперед и назад через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши курсора
    - параллельная парковка через клавиши 1 и 3
    - возврат серво в исходное положение пробелом или клавишей 2
    - управление серво через пробел - после любого из положений, сначала нажимаем пробел, затем другое положение

--- src/Sensors.py
+++ src/Sensors.py
@@ -1,36 +1,55 @@
+# Libraries
 import RPi.GPIO as GPIO
 import time
-# GPIO.cleanup()
 
+# GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
-TRIG = 23
-ECHO = 24
 
-GPIO.setwarnings(False)
+# set GPIO Pins
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-try:
-    while True:
-        # Minimum delay between measurements is 50ms. 100ms is fine:
-        time.sleep(0.1)
 
-        # Send a probe signal to the sensor. Signal should be 10 micro sec long.
-        GPIO.output(TRIG,True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG,False)
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
 
-    while GPIO.input(ECHO) == 0:
-        start = time.time()
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
 
-    while GPIO.input(ECHO) == 1:
-         stop = time.time()
+    StartTime = time.time()
+    StopTime = time.time()
 
-        # Print distance to object in santimeters. Sound speed = 340 m/s
-    print ("Distance = "),(stop - start) * 17000,"sm"
-    print ("start time = "), start
-    print ("stop time = "), stop
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
+            time.sleep(1)
+
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
