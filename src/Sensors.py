5cae8947d8e10812b5a785f75327215037d1a0aa
Denis Basharov <basharov@zirconb.com>
2021-04-03 13:46:24 +0300

    Основная рабочая версия Управление механикой Используется: - Pygame - для управления с клавиатуры и джойстика - Adafruit PCA9685 library
    Изменены названия переменных, отвечающих за значение PWM, убраны лишние, значение PWM начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает:
    - двежение вперед и назад через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши курсора
    - параллельная парковка через клавиши 1 и 3
    - возврат серво в исходное положение пробелом или клавишей 2
    - управление серво через пробел - после любого из положений, сначала нажимаем пробел, затем другое положение

---
+++ src/Sensors.py
@@ -0,0 +1,31 @@
+$cat 5_ultrasonic_distance_01.py
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+TRIG = 16
+ECHO = 18
+GPIO.setup(TRIG, GPIO.OUT, initial=0)
+GPIO.setup(ECHO, GPIO.IN)
+
+# Minimum delay between measurements is 50ms. 100ms is fine:
+time.sleep(0.1)
+
+# Send a probe signal to the sensor. Signal should be 10 micro sec long.
+GPIO.output(TRIG,1)
+time.sleep(0.00001)
+GPIO.output(TRIG,0)
+
+while GPIO.input(ECHO) == 0:
+pass
+start = time.time()
+
+while GPIO.input(ECHO) == 1:
+pass stop = time.time()
+
+# Print distance to object in santimeters. Sound speed = 340 m/s
+print "Distance = ",(stop - start) * 17000,"sm"
+print "start time = ", start
+print "stop time = ", stop
+
+GPIO.cleanup()
