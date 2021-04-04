157950bf02daac1146a29149b05f267843bd53fb
Denis Basharov <basharov@zirconb.com>
2021-04-04 18:30:04 +0300

    Основная рабочая версия Управление механикой Используется: - Pygame - для управления с клавиатуры и джойстика - Adafruit PCA9685 library
    Изменены названия переменных, отвечающих за значение PWM, убраны лишние, значение PWM начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает:
    - двежение вперед и назад через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши курсора
    - параллельная парковка через клавиши 1 и 3
    - возврат серво в исходное положение пробелом или клавишей 2
    - управление серво через пробел - после любого из положений, сначала нажимаем пробел, затем другое положение

--- src/mechanical_control.py
+++ src/mechanical_control.py
@@ -19,7 +19,32 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT) # на триггер назначаем ис
 GPIO.setup(GPIO_ECHO, GPIO.IN) # эхо делаем на прием
 
 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
 
+    # устанавливаем триггер через 0.01ms в состояние LOW (False или 0)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()  # время при отправке пакета
+    StopTime = time.time()  # время при приеме пакета
+
+    # сохраняем время старта
+    while GPIO.input(GPIO_ECHO) == 0:  # пока на эхо значение 0 записываем значение времени
+        StartTime = time.time()
+
+    # сохраняем время возвращения
+    while GPIO.input(GPIO_ECHO) == 1:  # пока на эхо значение 1 (возврат сигнала) записываем значение времени
+        StopTime = time.time()
+
+    # определяем разницу времени между стартом и возвращением
+    TimeElapsed = StopTime - StartTime  # определяем время прохождения сегнала от отправки до приема (проходит 2 расстояния - до объекта и обратно)
+    # время умножаем на скорость звука (34300 cm/s)
+    # и делим на 2, т.к. сигнал идет до препятствия, а затем возвращается
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
 
 
 
@@ -97,34 +122,11 @@ test_servo_left = test_servo_right = False
 
 while 1: # Запускаем общий цикл для всего - оптимизировать?
 
-    def distance():
-        # set Trigger to HIGH
-        GPIO.output(GPIO_TRIGGER, True)
-
-        # устанавливаем триггер через 0.01ms в состояние LOW (False или 0)
-        time.sleep(0.00001)
-        GPIO.output(GPIO_TRIGGER, False)
-
-        StartTime = time.time()  # время при отправке пакета
-        StopTime = time.time()  # время при приеме пакета
-
-        # сохраняем время старта
-        if GPIO.input(GPIO_ECHO) == 0:  # пока на эхо значение 0 записываем значение времени
-            StartTime = time.time()
-
-        # сохраняем время возвращения
-        if GPIO.input(GPIO_ECHO) == 1:  # пока на эхо значение 1 (возврат сигнала) записываем значение времени
-            StopTime = time.time()
-
-        # определяем разницу времени между стартом и возвращением
-        TimeElapsed = StopTime - StartTime  # определяем время прохождения сегнала от отправки до приема (проходит 2 расстояния - до объекта и обратно)
-        # время умножаем на скорость звука (34300 cm/s)
-        # и делим на 2, т.к. сигнал идет до препятствия, а затем возвращается
-        distance = (TimeElapsed * 34300) / 2
-
-        return distance
 
 
+
+    for distance in distance():
+        print (distance)
 
 
     for event in pygame.event.get():
