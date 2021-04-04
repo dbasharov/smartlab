966391c726936219525739cefddd46abc49dbddd
Denis Basharov <basharov@zirconb.com>
2021-04-04 18:13:43 +0300

    Основная рабочая версия Управление механикой Используется: - Pygame - для управления с клавиатуры и джойстика - Adafruit PCA9685 library
    Изменены названия переменных, отвечающих за значение PWM, убраны лишние, значение PWM начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает:
    - двежение вперед и назад через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши курсора
    - параллельная парковка через клавиши 1 и 3
    - возврат серво в исходное положение пробелом или клавишей 2
    - управление серво через пробел - после любого из положений, сначала нажимаем пробел, затем другое положение

--- src/mechanical_control.py
+++ src/mechanical_control.py
@@ -2,6 +2,53 @@ import pygame
 import time
 import Adafruit_PCA9685
 
+
+# Libraries
+import RPi.GPIO as GPIO
+import time
+
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO Pins
+GPIO_TRIGGER = 23 # пин на передачу на датчик
+GPIO_ECHO = 24 # пин на прием с датчика, на нем меряем время возврата сигнала
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT) # на триггер назначаем исходящий (1 или True - назначает 3,3 В на пине)
+GPIO.setup(GPIO_ECHO, GPIO.IN) # эхо делаем на прием
+
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # устанавливаем триггер через 0.01ms в состояние LOW (False или 0)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time() # время при отправке пакета
+    StopTime = time.time() # время при приеме пакета
+
+    # сохраняем время старта
+    while GPIO.input(GPIO_ECHO) == 0: # пока на эхо значение 0 записываем значение времени
+        StartTime = time.time()
+
+    # сохраняем время возвращения
+    while GPIO.input(GPIO_ECHO) == 1: # пока на эхо значение 1 (возврат сигнала) записываем значение времени
+        StopTime = time.time()
+
+    # определяем разницу времени между стартом и возвращением
+    TimeElapsed = StopTime - StartTime # определяем время прохождения сегнала от отправки до приема (проходит 2 расстояния - до объекта и обратно)
+    # время умножаем на скорость звука (34300 cm/s)
+    # и делим на 2, т.к. сигнал идет до препятствия, а затем возвращается
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+
+
+
+
 # import tkinter
 # import smbus
 #i2cBus = smbus.SMBus(1)
