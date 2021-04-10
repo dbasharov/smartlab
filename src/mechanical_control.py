d2f3dc24d4093ee1fe1d6044ab83fd0acadd2299
Denis Basharov <basharov@zirconb.com>
2021-04-10 08:35:57 +0300

    Основная рабочая версия Управление механикой - все задуманные режимы Начало автоматизации - работа с ультразвуковым датчиком расстояния
    Используется:
    - Pygame - для управления с клавиатуры и джойстика
    - Adafruit PCA9685 library
    - RPi.GPIO
    
    Первое значение PWM (№ pin, 0, хxxх) начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает механика:
    - двежение вперед и назад, повороты вправо и влево (по схеме автомобиль) через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши 1 и 2
    - параллельная парковка через клавиши 3 и 4
    - движение по кругу вокруг центра через клавиши 5 и 6
    - движение вокруг своего центра через клавишу 7 и танковый разворот 1 и 2
    - возврат серво в исходное положение пробелом
    
    Работает автоматизация:
    - сработка по датчику расстояния

--- src/mechanical_control.py
+++ src/mechanical_control.py
@@ -340,7 +340,7 @@ while 1: # Запускаем общий цикл для всего - оптим
 # ----------------------- автоматический режим
 
     dist = distance()
-    if dist > 20:
+    if dist > 30:
         # trig_dist_1 = True
         # flagUp = trig_dist_1
         print ("Measured Distance = %d cm" % dist)
@@ -352,7 +352,7 @@ while 1: # Запускаем общий цикл для всего - оптим
         wheel_1_fwd_pwm = wheel_2_fwd_pwm = wheel_3_fwd_pwm = wheel_4_fwd_pwm = speedUp
         print (wheel_1_fwd_pwm)
 
-    if  dist <= 20:
+    if  dist <= 30:
         print ("Measured Distance = %d cm" % dist)
         wheel_1_fwd_pwm = wheel_2_fwd_pwm = wheel_3_fwd_pwm = wheel_4_fwd_pwm = 0
         time.sleep(1)
