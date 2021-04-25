99b1df34fc21455dec1f63733e316e5cd34cdc86
Denis Basharov <basharov@zirconb.com>
2021-04-25 08:53:03 +0300

    Основная рабочая версия Управление механикой - все задуманные режимы Начало автоматизации - работа с ультразвуковым датчиком расстояния
    Используется:
    - Pygame - для управления с клавиатуры и джойстика
    - Adafruit PCA9685 library
    - RPi.GPIO
    
    Первое значение PWM (№ pin, 0, хxxх) начинается с 0, меняем только второе значение для изменения скорости двигателя.
    
    Работает механика:
    - двежение вперед и назад, повороты вправо и влево (по схеме автомобиль) через клавиши курсора
    - разворот на месте (танковый) налево и направо через клавиши 1 и 2
    - параллельная парковка через клавиши 3 и 4
    - движение по кругу вокруг центра через клавиши 5 и 6
    - движение вокруг своего центра через клавишу 7 и танковый разворот 1 и 2
    - возврат серво в исходное положение пробелом
    
    Работает автоматизация:
    - сработка по датчику расстояния

--- src/mechanical_control.py
+++ src/mechanical_control.py
@@ -269,36 +269,36 @@ try:
 
         print ("Расстояние с центрального УЗ датчика = %d cm" % dist_1)
 
-        if dist_1 > 30:
-            # trig_dist_1 = True
-            # flagUp = trig_dist_1
-            print ("Расстояние с центрального УЗ датчика больше 30 см = %d cm" % dist_1)
-            # print ("Measured Distance = %.1f cm" % dist_1)
-
-            speedUp = speedUp + 20
-            if speedUp > 4095:
-                speedUp = 4095
-            wheel_1_fwd_pwm = wheel_2_fwd_pwm = wheel_3_fwd_pwm = wheel_4_fwd_pwm = speedUp
-            print (wheel_1_fwd_pwm)
-
-        if  dist_1 <= 30:
-            print ("Расстояние с центрального УЗ датчика меньше 30 см = %d cm" % dist_1)
-            wheel_1_fwd_pwm = wheel_2_fwd_pwm = wheel_3_fwd_pwm = wheel_4_fwd_pwm = 0
-            # time.sleep(1)
-
-            # в print применен шаблон вывода данных, (метод format - сокращенно %)
-            # .1 - количество знаков после запятой, f - Float - дробные значения
-            # (могут быть d - числовое, s - строковое, i - целое числовое)
-
-
-            # range (200)
-            # print ("Measured Distance = %d cm" % dist_1)
-            # speedUp = speedUp_null
-            # speedUp = speedUp + 20
-            # if speedUp > 4095:
-            #     speedUp = 4095
-            # wheel_1_backward_pwm = wheel_2_backward_pwm = wheel_3_backward_pwm = wheel_4_backward_pwm = speedUp
-            # print (wheel_1_backward_pwm)
+        # if dist_1 > 30:
+        #     # trig_dist_1 = True
+        #     # flagUp = trig_dist_1
+        #     print ("Расстояние с центрального УЗ датчика больше 30 см = %d cm" % dist_1)
+        #     # print ("Measured Distance = %.1f cm" % dist_1)
+        #
+        #     speedUp = speedUp + 20
+        #     if speedUp > 4095:
+        #         speedUp = 4095
+        #     wheel_1_fwd_pwm = wheel_2_fwd_pwm = wheel_3_fwd_pwm = wheel_4_fwd_pwm = speedUp
+        #     print (wheel_1_fwd_pwm)
+        #
+        # if  dist_1 <= 30:
+        #     print ("Расстояние с центрального УЗ датчика меньше 30 см = %d cm" % dist_1)
+        #     wheel_1_fwd_pwm = wheel_2_fwd_pwm = wheel_3_fwd_pwm = wheel_4_fwd_pwm = 0
+        #     # time.sleep(1)
+        #
+        #     # в print применен шаблон вывода данных, (метод format - сокращенно %)
+        #     # .1 - количество знаков после запятой, f - Float - дробные значения
+        #     # (могут быть d - числовое, s - строковое, i - целое числовое)
+        #
+        #
+        #     # range (200)
+        #     # print ("Measured Distance = %d cm" % dist_1)
+        #     # speedUp = speedUp_null
+        #     # speedUp = speedUp + 20
+        #     # if speedUp > 4095:
+        #     #     speedUp = 4095
+        #     # wheel_1_backward_pwm = wheel_2_backward_pwm = wheel_3_backward_pwm = wheel_4_backward_pwm = speedUp
+        #     # print (wheel_1_backward_pwm)
 
 
         # if dist_1 >= 50:
